449675f2e94e22d7aad02ac975309ae6d76cce40
Mahdi Haroun <mhd0228222@ju.edu.jo>
2024-06-22 21:25:46 +0300

    idk

---
+++ Def_Functions.py
@@ -0,0 +1,10 @@
+def hello (): 
+    """This function prints Hello World!
+    some more documentation"""
+    print("Hello")
+    print("Glad to meet you")
+
+
+hello() # Call the function
+print("we are here ")
+hello() 
