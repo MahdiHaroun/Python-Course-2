cd846dd55a6889223e20e3d4abcd5af625078836
Mahdi Haroun <mhd0228222@ju.edu.jo>
2024-06-22 21:37:21 +0300

    Positional Parameter Passing end

---
+++ Positional Parameter Passing.py
@@ -0,0 +1,15 @@
+def hello2(s) : 
+    print("Hello " + s)
+    print("Glad to meet you")
+
+hello2("John") # Call the function
+hello2("Mary") 
+
+#**************************************
+def hello3(s1, s2) : 
+    greeting = "Hello" + s1 + "\n"
+    print(greeting*s2)
+
+
+hello3("John", 3) # Call the function
+hello3("Mary", 2)   
