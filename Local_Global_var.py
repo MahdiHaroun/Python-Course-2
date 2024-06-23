36786d4d81da676a3dfadd4dbee6fece0422bef4
Mahdi Haroun <mhd0228222@ju.edu.jo>
2024-06-23 12:37:00 +0300

    new file

--- Local_Global_var.py
+++ Local_Global_var.py
@@ -3,5 +3,5 @@ def square(x):
     return y
 
 z = square(10)
-print(y)     #error , y is not defined in the global scope , it is defined in the local scope of the function square
+# print(y)     #error , y is not defined in the global scope , it is defined in the local scope of the function square
 #**************************************
